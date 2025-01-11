b5d72ec01e086ab00d248726f14dee28ea1724b4
Bijaya Adhikari <bjadhs@gmail.com>
2025-01-11 23:51:25 +1100

    asyncHandler middleware

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,27 @@
+//Packages
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
+//Files
 import connectDB from "./db.js";
+import userRoute from "./routes/userRoute.js";
+import genreRoute from "./routes/genreRoute.js";
+import moviesRoute from "./routes/moviesRoute.js";
 
+//Configurations
 dotenv.config();
 connectDB();
 
+//Middlewares
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req,res) =>{
-    res.send("Server running");
-})
+//Routes
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/genres", genreRoute);
+app.use("/api/v1/movies", moviesRoute);
 
 const PORT = process.env.PORT || 5001;
 
---
+++ backend/middlewares/asyncHandler.js
@@ -0,0 +1,9 @@
+
+const asyncHandler = (fn) => (req,res,next) =>{
+    Promise.resolve(fn(req,res,next)).catch((error)=>{
+        console.log(`Error: ${error.message}`);
+        res.status(500).json({message: error.message});
+    });
+}
+
+export default asyncHandler;
